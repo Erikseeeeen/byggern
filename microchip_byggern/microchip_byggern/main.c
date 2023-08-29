b3431f3f09c7ec669e3d7fcaf9e4f365dbece3a4
Eivind Sunde Eriksen <eiviser@ntnu.no>
2023-08-29 08:46:55 +0200

    skeleton project

---
+++ microchip_byggern/microchip_byggern/main.c
@@ -0,0 +1,18 @@
+/*
+ * microchip_byggern.c
+ *
+ * Created: 29.08.2023 08:46:19
+ * Author : eiviser
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
