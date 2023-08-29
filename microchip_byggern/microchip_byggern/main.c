944510ef5e201a4617eb38ea7da25c08a4ff1d6a
Eivind Sunde Eriksen <eiviser@ntnu.no>
2023-08-29 12:59:20 +0200

    update usart

--- microchip_byggern/microchip_byggern/main.c
+++ microchip_byggern/microchip_byggern/main.c
@@ -7,16 +7,23 @@
 
 #include <avr/io.h>
 #include <util/delay_basic.h>
-
+#include "usart.h"
 
 int main(void)
 {
+	usart_init(USART_BAUD_RATE_REGISTER);
+	
     DDRA |= 1 << 0;
     /* Replace with your application code */
     while (1) 
     {
+		USART_Transmit('a');
         PORTA |= 1 << 0;
-        _delay_loop_2(65536);
+        _delay_loop_2(6553);
+        PORTA &= ~(1 << 0);
+        _delay_loop_2(6553);
     }
+	
+	
 }
 
--- microchip_byggern/microchip_byggern/usart.c
+++ microchip_byggern/microchip_byggern/usart.c
@@ -3,30 +3,30 @@
 void usart_init(unsigned int usart_baud_rate_register)
 {
     // Set baud rate registers
-    UBBRH = (unsigned char)(usart_baud_rate_register>>8); // usart_baud_rate_register_high
-    UBBRL = (unsigned char)usart_baud_rate_register; // usart_baud_rate_register_low
+    UBBR0H = (unsigned char)(usart_baud_rate_register>>8); // usart_baud_rate_register_high
+    UBBR0L = (unsigned char)usart_baud_rate_register; // usart_baud_rate_register_low
     
     // Enable RX and TX registers
-    UCSRB = (1<<RXEN) | (1<<TXEN); // Universal Synchronous Asynchronous Receiver Transmitter Register B, RX Enable, TX Enable
+    UCSR0B = (1<<RXEN0) | (1<<TXEN0); // Universal Synchronous Asynchronous Receiver Transmitter Register B, RX Enable, TX Enable
     
     // Set frame format to 8 data bits, 2 stop bits
-    UCSRC = (1<<URSEL)|(1<<USBS)|(3<<UCSZ0); // Universal Synchronous Asynchronous Receiver Transmitter Register C, USART Register Select, USART Stop Bit Select, USART Character Size Select
+    UCSR0C = (1<<URSEL0)|(1<<USBS0)|(3<<UCSZ0); // Universal Synchronous Asynchronous Receiver Transmitter Register C, USART Register Select, USART Stop Bit Select, USART Character Size Select
 
     
 }
 void USART_Transmit(unsigned char data)
 {
     // Wait until the transmitter buffer is empty
-    while ( !( UCSRA & (1<<UDRE)) ); // USART Control and Status Register A, USART Data Register Empty
+    while ( !( UCSR0A & (1<<UDRE0)) ); // USART Control and Status Register A, USART Data Register Empty
     // Send data
-    UDR = data; // USART Data Register
+    UDR0 = data; // USART Data Register
 }
 
 unsigned char USART_Receive(void)
 {
     // Wait for data to be received
-    if ( !(UCSRA & (1<<RXC)) ) // USART Control and Status Register A, USART Receive Complete
-        return '\0'
+    if ( !(UCSR0A & (1<<RXC0)) ) // USART Control and Status Register A, USART Receive Complete
+        return '\0';
     // Return data received
-    return UDR; // USART Data Register
+    return UDR0; // USART Data Register
 }
