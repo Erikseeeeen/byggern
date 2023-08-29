facb8ccc23211c1585cc05c955c5a17c49d974bb
Erikseeeeen <eivindseriksen@gmail.com>
2023-08-29 10:23:43 +0200

    Alternating pin 1 port a

--- microchip_byggern/microchip_byggern/main.c
+++ microchip_byggern/microchip_byggern/main.c
@@ -6,13 +6,17 @@
  */ 
 
 #include <avr/io.h>
+#include <util/delay_basic.h>
 
 
 int main(void)
 {
+    DDRA |= 1 << 0;
     /* Replace with your application code */
     while (1) 
     {
+        PORTA |= 1 << 0;
+        _delay_loop_2(65536);
     }
 }
 
