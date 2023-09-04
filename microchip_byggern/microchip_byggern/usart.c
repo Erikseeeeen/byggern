4458cc8b79e950c3c016800c8f5ec02c9c51d6cc
Erikseeeeen <eivindseriksen@gmail.com>
2023-09-04 10:15:17 +0200

    Untested implementation of printf

--- microchip_byggern/microchip_byggern/usart.c
+++ microchip_byggern/microchip_byggern/usart.c
@@ -1,7 +1,12 @@
 #include "usart.h"
 
+static FILE usart_stream_write = FDEV_SETUP_STREAM(USART_Transmit, NULL, _FDEV_SETUP_WRITE);
+static FILE usart_stream_read = FDEV_SETUP_STREAM(NULL, USART_Transmit, _FDEV_SETUP_READ);
+
 void usart_init(unsigned int usart_baud_rate_register)
 {
+    // Open read/write stream over USART https://www.nongnu.org/avr-libc/user-manual/group__avr__stdio.html#gab599ddf60819df4cc993c724a83cb1a4
+    fdevopen(usart_stream_write, usart_stream_read);
     // Set baud rate registers
     UBRR0H = (unsigned char)(usart_baud_rate_register>>8); // usart_baud_rate_register_high
     UBRR0L = (unsigned char)usart_baud_rate_register; // usart_baud_rate_register_low
@@ -13,6 +18,8 @@ void usart_init(unsigned int usart_baud_rate_register)
     // Set frame format to 8 data bits, 2 stop bits
     UCSR0C = (1<<URSEL0)|(1<<USBS0)|(3<<UCSZ00); // Universal Synchronous Asynchronous Receiver Transmitter Register C, USART Register Select, USART Stop Bit Select, USART Character Size Select
 
+    // Test printf
+    printf("Hello world");
     
 }
 void USART_Transmit(unsigned char data)
