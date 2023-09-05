b328145584f5fbd77f95890166a29312da204d4e
Erikseeeeen <eivindseriksen@gmail.com>
2023-09-05 09:09:48 +0200

    set JTAG mask registers

--- microchip_byggern/microchip_byggern/main.c
+++ microchip_byggern/microchip_byggern/main.c
@@ -12,14 +12,21 @@
 int main(void)
 {
 	usart_init(USART_BAUD_RATE_REGISTER);
+    // Mask out some port C address pins for JTAG (datasheet page 32)
+    // Write 1 to XMM0 register
+    SFIOR |= 1 << XMM0;
+    SFIOR &= ~(1 << XMM0 | 1 << XMM1);
 	
+    // Set port A pin 0 as output
     DDRA |= 1 << 0;
-    /* Replace with your application code */
+    
     while (1) 
     {
 	    USART_Transmit('a');
+        // Set port A pin 0 high
 		PORTA |= 1 << 0;
         _delay_loop_2(6553);
+        // Set port A pin 0 low
         PORTA &= ~(1 << 0);
         _delay_loop_2(6553);
     }
