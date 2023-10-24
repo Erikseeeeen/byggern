798fc2de6a30352c78aa4ac71e18919f845eb449
Jørgen Havig <jorgehav@ntnu.no>
2023-10-24 14:05:41 +0200

    adc stub

---
+++ node_2/node_2/adc.h
@@ -0,0 +1,18 @@
+/*
+ * adc.h
+ *
+ * Created: 24.10.2023 13:00:56
+ *  Author: jorgehav
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+# include "sam.h"
+
+
+void adc_init();
+
+
+#endif /* ADC_H_ */
