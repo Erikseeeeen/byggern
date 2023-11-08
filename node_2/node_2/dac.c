86a36367efbf88851ab0fbdcc574a8180edea86b
Eivind Sunde Eriksen <eiviser@ntnu.no>
2023-11-08 17:29:41 +0100

    implement dac write speed

--- node_2/node_2/dac.c
+++ node_2/node_2/dac.c
@@ -12,19 +12,21 @@ void dac_init()
 	// Enable channel
 	DACC->DACC_CHER = DACC_CHER_CH1;
 	
-	dac_write_voltage(0xFFFF);
-	dac_write_voltage(0);
+	dac_write_uint_voltage(0xFFFF);
+	dac_write_uint_voltage(0);
 	
 }
 
-void dac_write_uint_voltage(uint16_t normalized_signal)
+void dac_write_uint_voltage(uint16_t signal)
 {
 	DACC->DACC_CDR = signal;
 }
 
 
-void dac_write_speed(float normalized_signal)
+void dac_write_speed()
 {
+	float normalized_signal = (float)(joystick_y) / 128.0 - 1.0;
+	
 	if (normalized_signal < -1)
 		normalized_signal = -1;
 	if (normalized_signal > 1)
@@ -32,12 +34,14 @@ void dac_write_speed(float normalized_signal)
 		
 	if(normalized_signal < 0)
 	{
-		dac_write_uint_voltage((uint16_t)(-signal * 65535))
+		dac_write_uint_voltage((uint16_t)(-normalized_signal * 65535));
 		// Set motor direction left
+		PIOD->PIO_CODR = PIO_PD10;
 	}
 	else
 	{
-		dac_write_uint_voltage((uint16_t)(signal * 65535))
+		dac_write_uint_voltage((uint16_t)(normalized_signal * 65535));
 		// Set motor direction right
+		PIOD->PIO_SODR = PIO_PD10;
 	}
 }
--- node_2/node_2/dac.h
+++ node_2/node_2/dac.h
@@ -3,10 +3,13 @@
 
 #include "sam.h"
 
+uint8_t joystick_y;
+
 void dac_init();
 
 void dac_write_voltage(uint16_t signal);
 
+void dac_write_speed();
 
 
 #endif /* DAC_H_ */
